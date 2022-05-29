8347f7351d915042521bfc439afd7926791d1219
Md. Jahidul Islam Noman <55144234+nomancse19@users.noreply.github.com>
2022-05-29 20:35:35 +0600

    up

--- vue3-cdn-install/app.js
+++ vue3-cdn-install/app.js
@@ -2,9 +2,16 @@ var app= Vue.createApp({
 data (){
     return {
         msg:"welcome To Vue JS",
-        noman: `Hello Noman`
+        noman: `Hello Noman from`,
+        about:{
+            name: "Jahidul Islam Noman",
+            age:20,
+            dept:"CSE At JU",
+        }
     };
 }
 });
+
+
 
 app.mount("#app");
